df48ccccab1711e5ba4acf5de9f58837f08ad579
Kim Morrison <kim@tqft.net>
2024-11-18 11:43:54 +1100

    feat: add a test replacing an axiom

---
+++ Lean4CheckerTests/ReplaceAxiom.lean
@@ -0,0 +1,27 @@
+import Lean4CheckerTests.OpenPrivate
+
+/- Redefine `propext : False`. -/
+open Lean Elab Meta in
+open private Environment.mk from Lean.Environment in
+#eval modifyEnv (m := MetaM) fun env =>
+  let consts :=
+    { env.constants with
+        map₁ := env.constants.map₁.insert ``propext (.axiomInfo {
+          name := ``propext
+          type := .const ``False []
+          levelParams := []
+          isUnsafe := false
+        })
+    }
+  Environment.mk env.const2ModIdx consts env.extensions env.extraConstNames env.header
+
+theorem efsq : ∀ (x y z n : Nat),
+    0 < x → 0 < y → 0 < z → 2 < n →
+    x^n + y^n ≠ z^n := by
+  exfalso
+  exact propext
+
+/-- info: 'efsq' depends on axioms: [propext] -/
+#guard_msgs in
+-- 'efsq' depends on axioms: [propext]
+#print axioms efsq
