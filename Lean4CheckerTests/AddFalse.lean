0bbe3581e183bdd4aa28d0ebc8bad45edd92d5d9
Sebastian Ullrich <sebasti@nullri.ch>
2025-03-13 21:42:23 +0100

    fix

--- Lean4CheckerTests/AddFalse.lean
+++ Lean4CheckerTests/AddFalse.lean
@@ -1,16 +1,15 @@
 import Lean4CheckerTests.OpenPrivate
 
-open private Lean.Environment.mk from Lean.Environment
+open private Lean.Environment.updateBaseAfterKernelAdd from Lean.Environment
 open private Lean.Kernel.Environment.mk from Lean.Environment
 open private Lean.Kernel.Environment.extensions from Lean.Environment
 open private Lean.Kernel.Environment.extraConstNames from Lean.Environment
-open private Lean.AsyncConsts from Lean.Environment
 
 open Lean in
 elab "add_false" : command => do
   modifyEnv fun env =>
-    let constants := env.constants.insert `false $ ConstantInfo.thmInfo
-      { name := `false, levelParams := [], type := .const ``False [], value := .const ``False [] }
+    let info := { name := `false, levelParams := [], type := .const ``False [], value := .const ``False [] }
+    let constants := env.constants.insert `false $ ConstantInfo.thmInfo info
     -- Before `Environment.mk` became private, we could just use
     -- `{ env with constants }`
     let kenv := Lean.Kernel.Environment.mk constants
@@ -20,7 +19,9 @@ elab "add_false" : command => do
       (Lean.Kernel.Environment.extensions env.toKernelEnv)
       (Lean.Kernel.Environment.extraConstNames env.toKernelEnv)
       env.header
-    Lean.Environment.mk kenv (.pure kenv) default none none {}
+    -- only the name matters on the elab level
+    let decl := .axiomDecl { info with isUnsafe := false }
+    Lean.Environment.updateBaseAfterKernelAdd env kenv decl
 
 add_false
 
--- Lean4CheckerTests/AddFalseConstructor.lean
+++ Lean4CheckerTests/AddFalseConstructor.lean
@@ -1,6 +1,6 @@
 import Lean4CheckerTests.OpenPrivate
 
-open private Lean.Environment.mk from Lean.Environment
+open private Lean.Environment.updateBaseAfterKernelAdd from Lean.Environment
 open private Lean.Kernel.Environment.mk from Lean.Environment
 open private Lean.Kernel.Environment.extensions from Lean.Environment
 open private Lean.Kernel.Environment.extraConstNames from Lean.Environment
@@ -8,15 +8,18 @@ open private Lean.Kernel.Environment.extraConstNames from Lean.Environment
 open Lean in
 elab "add_false_constructor" : command => do
   modifyEnv fun env =>
-    let constants := env.constants.insert `False.intro $ ConstantInfo.ctorInfo
-      { name := `False.intro
-        levelParams := []
-        type := .const ``False []
-        induct := `False
-        cidx := 0
-        numParams := 0
-        numFields := 0
-        isUnsafe := false }
+    let info := {
+      name := `False.intro
+      levelParams := []
+      type := .const ``False []
+      induct := `False
+      cidx := 0
+      numParams := 0
+      numFields := 0
+      isUnsafe := false
+    }
+    let constants := env.constants.insert `False.intro $ ConstantInfo.ctorInfo info
+
     -- Before `Environment.mk` became private, we could just use
     -- `{ env with constants }`
     let kenv := Lean.Kernel.Environment.mk constants
@@ -26,7 +29,9 @@ elab "add_false_constructor" : command => do
       (Lean.Kernel.Environment.extensions env.toKernelEnv)
       (Lean.Kernel.Environment.extraConstNames env.toKernelEnv)
       env.header
-    Lean.Environment.mk kenv (.pure kenv) default none none {}
+    -- only the name matters on the elab level
+    let decl := .axiomDecl { info with }
+    Lean.Environment.updateBaseAfterKernelAdd env kenv decl
 
 add_false_constructor
 
